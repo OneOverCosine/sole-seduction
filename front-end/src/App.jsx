9140f69015f034c2dffa3daab1446fcf9dbd2ea7
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-05-20 12:05:27 +0100

    Hero Image added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 import HomePage from './components/HomePage/HomePage'
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
--- front-end/src/components/HomePage/HeroImage.jsx
+++ front-end/src/components/HomePage/HeroImage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import Image from 'react-bootstrap';
+import Image from 'react-bootstrap/Image';
+import './HeroImage.css';
+import converse from '../../assets/hero-converse.png';
+import heels from '../../assets/hero-heels.png';
+import nike from '../../assets/hero-nike.png';
 
 const HeroImage = () => {
+
+    const randomImage = () => {
+        const rand = Math.floor(Math.random() * 3);
+
+        if (rand === 0)
+            return converse;
+        if (rand === 1)
+            return heels;
+        else return nike;
+    }
+
     return (
         <>
-            <div>HeroImage</div>
-            <Image />
+            <Image fluid='true' className='hero-image' src={randomImage()} />
         </>
     )
 }

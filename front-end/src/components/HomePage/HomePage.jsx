69b2fca51ca02eafb56f8cf2279aaecf063f78e4
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-05-20 13:25:30 +0100

    Footer added

--- front-end/src/components/HomePage/HomePage.jsx
+++ front-end/src/components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import HeroImage from "./HeroImage";
 import Categories from "./Categories";
+import Footer from "../NavBar/Footer";
 
 const HomePage = () => {
     return (
         <>
             <HeroImage />
             <Categories />
+            <Footer />
         </>
     )
 }
---
+++ front-end/src/components/NavBar/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='p-2 bg-dark text-light'>An e-Commerce App created by Wil, Dunni and Paula</div>
+    )
+}
+
+export default Footer

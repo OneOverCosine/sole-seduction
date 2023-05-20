bfaa4824e7f2996cc25848c0d2ee42d984b54f39
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-05-20 13:19:25 +0100

    NavBar added

--- front-end/src/components/NavBar/NavBar.jsx
+++ front-end/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+import logo from '../../assets/logo.png';
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt="sole seduction logo a shoe with a heart"
+                            src={logo} // Stolen from the internet
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                        Sole Seduction
+                    </Navbar.Brand>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <NavDropdown title="Men" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">
+                                    Sport
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Formal
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something else</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Women" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">
+                                    Sport
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Formal
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something else</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link className='text-danger' href="#sale">Sale</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 

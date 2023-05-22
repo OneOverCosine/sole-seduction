d7437528a28f4e135bd4b9eb1027570872015539
Paula Gayle <paula_gayle@hotmail.com>
2023-05-22 23:26:32 +0100

    react-bootstrap first attempt

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/productpage" element={<ProductPage />} />
-                    {/* <Route path="/checkout" element={<CheckoutPage />} /> */}
                                        
                 </Routes>
                 <Footer />
--- front-end/src/components/ProductPage/ProductPage.jsx
+++ front-end/src/components/ProductPage/ProductPage.jsx
@@ -1,10 +1,21 @@
 import ProductImages from "./ProductImages";
 import ProductDescription from "./ProductDescription";
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
 
 const ProductPage = () => {
 
     return (
         <>
+            {/* <Container>
+                <Row>
+                    <Col sm={6}><ProductImages /></Col> 
+                    <Col sm={6}><ProductDescription /></Col>
+                </Row>
+            </Container> */}
+
             <ProductImages />
             <ProductDescription />
         </>

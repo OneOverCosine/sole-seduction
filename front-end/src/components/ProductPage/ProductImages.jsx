c8f79510415ef6ac823615e9955839e049545dfb
Paula Gayle <paula_gayle@hotmail.com>
2023-05-24 09:19:37 +0100

    active thumbnails

--- front-end/src/components/ProductPage/ProductImages.jsx
+++ front-end/src/components/ProductPage/ProductImages.jsx
@@ -1,23 +1,29 @@
+import { useState } from 'react';
 import './productPage.css';
 
 const ProductImages = (props) => {  
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    const handleTab = () => {
+    const handleThumbnail = (index) => {
+        setCurrentImageIndex(index);
 
+        const galleryImages = document.querySelectorAll(".gallery-image img");
+        galleryImages[currentImageIndex].classList.remove("active");
+        galleryImages[index].classList.add("active");    
     }
 
     return (
         <>
             <div className="images">
                 <div className="main-image">
-                    <img src={props.img[0]} alt="product image" />
+                    <img src={props.img[currentImageIndex]} alt="product image" />
                 
                 </div>
 
                 <div className="gallery-image">
                     {
-                        props.img.map(image => (
-                            <img src={image} alt="thumbnail" onClick={ handleTab }/>
+                        props.img.map((image, index) => (
+                            <img src={image} alt="thumbnail" onClick={ () => handleThumbnail(index) } key={index}/>
                             
                         ))
                     }

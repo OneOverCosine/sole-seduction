63a7cfe814d0de006489ad9753e1e404c567c073
OneOverCosine <TheCandyDragon@gmail.com>
2023-05-30 10:38:41 +0100

    quick update

--- back-end/src/routes/categories.route.js
+++ back-end/src/routes/categories.route.js
@@ -2,7 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-/* basic search page for all products */
+/* basic search page for all products
+also used to filter products by brand and all that*/
 
 router.route(`/`).get(async (req, res) => {
 

6cc41ac894b14f9b53f0bfe5da182e2c90f8ab02
OneOverCosine <TheCandyDragon@gmail.com>
2023-06-01 10:48:24 +0100

    added connection to correct document

--- back-end/src/models/product_data.model.js
+++ back-end/src/models/product_data.model.js
@@ -1,17 +1,25 @@
 import { Decimal128 } from "mongodb";
 import mongoose from "mongoose";
 
+const inventorySchema = new mongoose.Schema(
+    {
+        size: Number,
+        colour: String,
+        quantity: Number
+    }
+);
+
 const productDataSchema = new mongoose.Schema({
-    brand: { type: String },
-    model: { type: String },
-    gender: { type: String },
-    img: { type: Array },
-    description: { type: String },
-    price: { type: Decimal128 },
-    colours: { type: Array },
-    sizes: { type: Array },
-    inventory: { type: Array }
-});
+    brand: String,
+    model: String,
+    gender: String,
+    img: Array,
+    description: String,
+    price: Decimal128,
+    colours: Array,
+    sizes: Array,
+    inventory: [inventorySchema]
+}, { collection: 'product_data' });
 
 const ProductData = mongoose.model(`product_data`, productDataSchema);
 

842451563508d0cfb977d0783de36ce3441de1b3
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-07-29 13:14:02 +0100

    Database connection changes for Cyclic

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,34 +1,32 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Footer from './components/NavBar/Footer';
-import HomePage from './components/HomePage/HomePage';
-import ProductPage from './components/ProductPage/ProductPage';
-import CheckoutPage from './components/CheckoutPage/CheckoutPage';
-import NavBar from './components/NavBar/NavBar';
-import CategoryPage from './components/CategoryPage/CategoryPage';
-import ShopContextProvider from './context/ShopContext';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/NavBar/Footer";
+import HomePage from "./components/HomePage/HomePage";
+import ProductPage from "./components/ProductPage/ProductPage";
+import CheckoutPage from "./components/CheckoutPage/CheckoutPage";
+import NavBar from "./components/NavBar/NavBar";
+import CategoryPage from "./components/CategoryPage/CategoryPage";
+import ShopContextProvider from "./context/ShopContext";
 
 function App() {
+  return (
+    <>
+      <ShopContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/categories" element={<CategoryPage />} />
+            <Route path="/product/:productId" element={<ProductPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+          </Routes>
 
-    return (
-        <>
-            <ShopContextProvider>
-                <BrowserRouter>
-                    <NavBar />
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/categories" element={<CategoryPage />} />
-                        <Route path="/product/:productId" element={<ProductPage />} />
-                        <Route path="/checkout" element={<CheckoutPage />} />
-                    </Routes>
-
-                    <Footer />
-                </BrowserRouter>
-            </ShopContextProvider>
-        </>
-    )
+          <Footer />
+        </BrowserRouter>
+      </ShopContextProvider>
+    </>
+  );
 }
 
-export default App
+export default App;

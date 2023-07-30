aa7458db32b5ca2c7099ed32cdcece59abf70eaa
Wil Treasure <100243250+treasurewild@users.noreply.github.com>
2023-07-30 20:52:19 +0100

    Preventing add to cart if no selection is made

--- front-end/src/components/ProductPage/ProductDescription.jsx
+++ front-end/src/components/ProductPage/ProductDescription.jsx
@@ -1,74 +1,76 @@
-import './productPage.css';
-import {ShopContext} from '../../context/ShopContext'
-import { useContext, useState } from 'react';
+import "./productPage.css";
+import { ShopContext } from "../../context/ShopContext";
+import { useContext, useState } from "react";
 
 const ProductDescription = (props) => {
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const [selectedColour, setSelectedColour] = useState("");
+  const [selectedSize, setSelectedSize] = useState("");
 
-    const { addToCart, cartItems } = useContext(ShopContext)
-    const [selectedColour, setSelectedColour] = useState('');
-    const [selectedSize, setSelectedSize] = useState('');
+  const cartItemAmount = cartItems[props.id];
 
-    const cartItemAmount = cartItems[props.id]
-    
-    const handleAddToCart = () => {
-        addToCart(props.id, selectedColour, selectedSize);
-    };
-    
-    const handleColorSelection = (colour) => {
-        setSelectedColour(colour);
-    };
+  const handleAddToCart = () => {
+    addToCart(props.id, selectedColour, selectedSize);
+  };
 
-    const handleSizeSelection = (size) => {
-        setSelectedSize(size);
-    };
+  const handleColorSelection = (colour) => {
+    setSelectedColour(colour);
+  };
 
-    return (
-        <div className="details">
-            <div className="box">
-                <div className="row">
-                    <h2>{props.model}</h2>
-                    <span>£{props.price}</span>
-                </div>
+  const handleSizeSelection = (size) => {
+    setSelectedSize(size);
+  };
 
-                <div className="colours">
-                    {
-                        props.colours.map(colour => (
-                            <button
-                                key={colour}
-                                style={{
-                                    background: colour,
-                                    border: selectedColour === colour ? "3px solid lightseagreen" : "1px solid #333"
-                                }}
-                                onClick={() => handleColorSelection(colour)}
-                                >
-                                
-                            </button>
-                        ))
-                    } 
-                </div>
-
-                <div className="sizes">
-                    {
-                        props.sizes.map(size => (
-                            <button
-                                key={size}
-                                style={{
-                                    border: selectedSize === size ? "3px solid lightseagreen" : "none"
-                                }}
-                                onClick={() => handleSizeSelection(size)}>
-                                {size}
-                            </button>
-                        ))
-                    }
-                </div>
-                       
-                <p className="description">{props.description}</p>
-
-                <button className="cart-btn" onClick={handleAddToCart}>
-                    Add to cart {cartItemAmount > 0 && <> ({cartItemAmount}) </>}
-                </button>
-            </div>
+  return (
+    <div className="details">
+      <div className="box">
+        <div className="row">
+          <h2>{props.model}</h2>
+          <span>£{props.price}</span>
         </div>
-    )
-}
-export default ProductDescription
+
+        <div className="colours">
+          {props.colours.map((colour) => (
+            <button
+              key={colour}
+              style={{
+                background: colour,
+                border:
+                  selectedColour === colour
+                    ? "3px solid lightseagreen"
+                    : "1px solid #333",
+              }}
+              onClick={() => handleColorSelection(colour)}
+            ></button>
+          ))}
+        </div>
+
+        <div className="sizes">
+          {props.sizes.map((size) => (
+            <button
+              key={size}
+              style={{
+                border:
+                  selectedSize === size ? "3px solid lightseagreen" : "none",
+              }}
+              onClick={() => handleSizeSelection(size)}
+            >
+              {size}
+            </button>
+          ))}
+        </div>
+
+        <p className="description">{props.description}</p>
+
+        <button
+          className="cart-btn"
+          disabled={!selectedColour && !selectedSize}
+          onClick={handleAddToCart}
+        >
+          Add to cart {cartItemAmount > 0 && <> ({cartItemAmount}) </>}
+        </button>
+      </div>
+    </div>
+  );
+};
+export default ProductDescription;
